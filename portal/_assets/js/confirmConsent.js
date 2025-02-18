36cc73fd22a2f788a3bdcf653cad0d5ded2644c0
Chris Dreyße <chrisdreysse@gmail.com>
2025-02-18 12:47:57 +0100

    toggle button to give or revoke consent

--- portal/_assets/js/confirmConsent.js
+++ portal/_assets/js/confirmConsent.js
@@ -5,5 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('cookieOk').addEventListener('click', function() {
       // signal to matomo that consent was given
       _paq.push(['rememberConsentGiven']);
+      // safe the state
+      localStorage.setItem("trackingConsent", "given");
     });
 });
--- portal/_assets/js/withdrawConsent.js
+++ portal/_assets/js/withdrawConsent.js
@@ -1,10 +1,35 @@
+
 // callback if document is fully loaded
 document.addEventListener("DOMContentLoaded", function() { 
   
-    // eventListener if consent removke button was clicked
-    document.getElementById('revokeConsentButton').addEventListener('click', function() {
-      // signal to matomo that consent was given
-      _paq.push(['forgetConsentGiven']);
-      alert("Tracking-Consent erfolgreich widerrufen.");
-    });
-});
+    // localstorage to safe state in the browser if user has given consent or not
+    var consentGiven = localStorage.getItem("trackingConsent") === "given";
+  
+    var toggleButton = document.getElementById("toggleConsent"); 
+  
+    // button text according to the state
+    if(consentGiven) {
+      toggleButton.textContent = "Tracking deaktivieren"
+    } else {
+      toggleButton.textContent = "Tracking erlauben"
+    }
+  
+    // event listener for the button
+    toggleButton.addEventListener("click", function() {
+      if(consentGiven) {
+        // withdraw consent - change button text - update consentGiven
+        _paq.push(['forgetConsentGiven']); 
+        alert("Tracking-Consent erfolgreich widerrufen.")
+        localStorage.setItem("trackingConsent", "revoked");
+        toggleButton.textContent = "Tracking erlauben";
+        consentGiven = false; 
+      } else {
+        // give consent - change button text - update consentGiven
+        _paq.push(['rememberConsentGiven']); 
+        alert("Tracking-Consent erfolgreich erteilt."); 
+        localStorage.setItem("trackingConsent", "given")
+        toggleButton.textContent = "Tracking deaktivieren"
+        consentGiven = true; 
+      }
+    })
+  });
